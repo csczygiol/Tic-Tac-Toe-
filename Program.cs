03da2e032cdda645be6a4f4c60494215eddb11b6
K_Claude <csczygiol@googlemail.com>
2019-06-07 17:27:56 +0200

    Core Functionality_1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace Tic_Tac_Toe
     {
         static void Main(string[] args)
         {
+            int column = 3;
+            int row = 3;
+            int[,] table = new int[column, row];
+
+
+            for( int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+
+                }
+            }
+                
+
+
+            //Console.WriteLine("Hello Player {0} it is your turn to select a column");
+            //Console.WriteLine("Hello Player {0} it is your turn to select a row");
         }
     }
 }
